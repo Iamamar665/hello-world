b3db364745ed242fe36f156d9558ba8131f41d79
Iamamar665 <64791435+Iamamar665@users.noreply.github.com>
2020-05-06 22:44:26 +0530

    Update fibonaccci numbers.py

--- fibonaccci numbers.py
+++ fibonaccci numbers.py
@@ -1,11 +1,20 @@
-print(0)
-print(1)
-i = 0
-j = 1
-while i+j>0:
-    k=i+j
-    print(k)
-    i=j
-    j=k
-    if k>10000:
-        break
+#taking the input from user
+N = int(input("how many fibonacci numbers do you want to print"))
+#declaring variables
+n1,n2 = 0,1
+#condition statements
+if N<=0:
+    print("please enter positive integer greater than zero")
+elif N ==1:
+    print("The fibonaaci numbers upto", N, "terms are: \n", n1)
+else:
+    print("The fibonaaci numbers upto", N, "terms are:")
+    count=0
+    while count<N:
+        print(n1)
+        #update variable values
+        n3=n1+n2
+        n1=n2
+        n2=n3
+        count+=1
+        
